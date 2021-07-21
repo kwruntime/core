63ea7e49507b5a97ef196acaf9bfe02f06c4de5c
James <xtreme.maicolxt94@gmail.com>
2021-07-21 03:57:48 +0000

    Fix addpath windows

--- Kawix.ts
+++ Kawix.ts
@@ -215,7 +215,7 @@ export class Installer{
 
             // setx path
             let child= require("child_process")
-            child.exec_sync("setx", ["path", bin + ";%path%"])
+            child.execSync("setx", ["path", bin + ";%path%"])
 
         }
 
@@ -242,7 +242,7 @@ export class Installer{
 
         }
 
-        
+
 
 
     }
